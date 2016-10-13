06a03abf6613cfdcb19f287cb9dc2aa967a6bb4f
taggartperez <taggartperez@marshall.edu>
2016-10-13 09:35:05 -0400

    Add project files.

---
+++ SWEET4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SWEET4.Startup))]
+namespace SWEET4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
