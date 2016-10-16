c83ae18cbc756494ea308b6acd77a62510abee10
taggartperez <taggartperez@marshall.edu>
2016-10-16 14:13:52 -0400

    Enabled Migrations so the project would work.

---
+++ SWEET4/Models/Appointment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SWEET4.Models
+{
+    public class Appointment
+    {
+        public int ID { get; set; }
+        public string Date { get; set; }
+    }
+}
